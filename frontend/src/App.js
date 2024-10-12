7ea9feee2b9fbaca371d33e91aae545cb945b4bc
Bhargav Gembali <113098503+BhargavGembali@users.noreply.github.com>
2024-10-12 22:29:39 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> GPUG OFFICIAL WEBSITE </h1> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
