ce2e371bb3e0febad590f0c1bffbcf2bd2f505de
synapticsurge <synapticsurge@proton.me>
2024-10-13 21:52:48 +0530

    tailwind config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>A Website For GPUG Club</h1>
+<h1 class="font-mono underline" >A Website For GPUG Club</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
